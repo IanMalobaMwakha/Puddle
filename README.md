3890730f29accfeed20030b1d0739e4ef67e7bae
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-05-06 22:53:42 +0300

    Created model Item and modified model Category Adding class Meta and Function to call name ...

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Puddle
 <p>This is an online marketplace website where people can buy and sell new and used items.</p>
-<p>It is built entirely by Python-Django, Tailwind CSS, HTML5, Javascript and Bootsrap.</p>
+<p>It is built entirely by Python-Django, Tailwind CSS, HTML5, Javascript and Bootstrap</p>
 
 
--- puddle/items/models.py
+++ puddle/items/models.py
@@ -2,3 +2,18 @@ from django.db import models
 
 class Cartegory(models.Model):
     name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    is_sold = models.BooleanField(default=False)
+    
