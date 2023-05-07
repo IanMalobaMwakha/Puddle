2b44ef5b209cb5578b5b1a37068902ef297d5d5d
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-05-07 13:44:37 +0300

    Modified views for the Items

--- puddle/items/models.py
+++ puddle/items/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Cartegory(models.Model):
@@ -12,8 +13,14 @@ class Cartegory(models.Model):
 
 
 class Item(models.Model):
+    cartegory = models.ForeignKey(Cartegory, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
     is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return self.name
--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATIC_URL = '/static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
