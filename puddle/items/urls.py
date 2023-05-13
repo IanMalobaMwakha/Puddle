a402e8a63708c3ed8a99e337ffc9d12960eb5f00
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-05-13 03:58:51 +0300

    modified:   puddle/items/templates/item/detail.html modified:   puddle/items/urls.py

--- puddle/items/urls.py
+++ puddle/items/urls.py
@@ -1,9 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
 
 app_name = 'item'
 
+
 urlpatterns = [
-    path('<int:pk>/', views.detail, name='detail')
-]
+    path('<int:pk>/', views.detail, name='detail'),
+] 
+
+
