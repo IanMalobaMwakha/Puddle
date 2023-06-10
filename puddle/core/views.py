536b9e04319a7a742d1fe7b521cbb45103791e8a
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-06-10 14:05:40 +0300

    modified:   puddle/core/views.py

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from items.models import Cartegory, Item
 
@@ -11,13 +11,21 @@ def index(request):
     return render(request, 'core/index.html',{
         'categories': categories,
         'items': items,
-    })
- 
+    }) 
 
 def contact(request):
     return render(request, 'core/contact.html')
 
 def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        
+        if form.is_valid():
+            form.save
+
+            return redirect('/login/')
+    else:
+        form = SignupForm()
     form = SignupForm()
 
     return render(request, 'core/signup.html', {
