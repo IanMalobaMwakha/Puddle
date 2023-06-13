f8effa68b1446d26c7a63c555efb727d307b5262
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-06-13 12:52:45 +0300

    modified:   puddle/items/__pycache__/forms..... modified:   puddle/items/forms.py

--- puddle/items/forms.py
+++ puddle/items/forms.py
@@ -2,12 +2,14 @@ from django import forms
 
 from .models import Item
 
+INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
+
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ('cartegory', 'name', 'description', 'price', 'image',)
         widgets = {
             'cartegory': forms.Select(attrs={
-                'class': 'w-full py-4 px-6 rounded-xl border'
+                'class': INPUT_CLASSES
             })
         }
