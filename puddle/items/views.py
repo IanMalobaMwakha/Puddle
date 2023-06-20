4a922af185b0df9395261a872076928a24d985f1
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-06-20 15:01:26 +0300

    modified:   puddle/items/__pycache__/views...... modified:   puddle/items/templates/item/detail.html modified:   puddle/items/views.py

--- puddle/items/views.py
+++ puddle/items/views.py
@@ -19,12 +19,12 @@ def new(request):
     if request.method == 'POST':
         form = NewItemForm(request.POST, request.FILES)
 
-        if form.is_valid:
+        if form.is_valid():
             item = form.save(commit=False)
             item.created_by = request.user
             item.save()
 
-            return redirect('items:detail', pk=item.id)
+            return redirect('item:detail', pk=item.id)
     else:
         form = NewItemForm()
 
