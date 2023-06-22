5764249dc532ffbc43cf90af73624fd59e3fcfc0
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-06-23 01:09:22 +0300

    modified:   puddle/items/__pycache__/views.cpython-310.pyc modified:   puddle/items/views.py

--- puddle/items/views.py
+++ puddle/items/views.py
@@ -38,14 +38,14 @@ def new(request):
 def edit(request, pk):
     item = get_object_or_404(Item, pk=pk, created_by=request.user)
     if request.method == 'POST':
-        form = EditItemForm(request.POST, request.FILES)
+        form = EditItemForm(request.POST, request.FILES, instance=item)
 
         if form.is_valid():
             form.save()
 
             return redirect('items:detail', pk=item.id)
     else:
-        form = EditItemForm()
+        form = EditItemForm(instance=item)
 
     return render(request, 'item/form.html',{
         'form':form,
