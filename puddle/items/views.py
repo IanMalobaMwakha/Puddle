563f6c03e81b087e3de047bfa151847ae4c5ec87
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-06-24 23:59:52 +0300

    modified:   puddle/items/views.py modified:   puddle/db.sqlite3 modified:   puddle/items/__pycache__/urls.cpython-310.pyc modified:   puddle/items/urls.py

--- puddle/items/views.py
+++ puddle/items/views.py
@@ -4,6 +4,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .forms import NewItemForm, EditItemForm
 from .models import Item
 
+def items(request):
+    items = Item.objects.filter(is_sold=False)
+
+    return render(request, 'item/items.html', {
+        'items': items,
+    })
+
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(cartegory=item.cartegory, is_sold=False).exclude(pk=pk)[0:3]
