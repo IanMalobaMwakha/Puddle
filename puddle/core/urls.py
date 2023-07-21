5aee24745ce3cfd5aff3f2dceca755b812946639
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-07-21 18:56:25 +0300

    modified:   puddle/core/__pycache__/urls.cpython-310.pyc modified:   puddle/core/__pycache__/views.cpython-310.pyc new file:   puddle/core/templates/core/about.html modified:   puddle/core/templates/core/base.html modified:   puddle/core/templates/core/contact.html modified:   puddle/core/urls.py modified:   puddle/core/views.py

--- puddle/core/urls.py
+++ puddle/core/urls.py
@@ -9,6 +9,7 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout_view, name='logout_view'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form = LoginForm), name='login'),
