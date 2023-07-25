10e459c33ab2b0caddb64096d143f63500b6a7ee
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-07-26 01:44:12 +0300

    modified:   puddle/items/templates/item/detail.html

--- puddle/core/models.py
+++ puddle/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='items', on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='profile_avatar', blank=True, null=True)
+    location = models.CharField(max_length=255, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
