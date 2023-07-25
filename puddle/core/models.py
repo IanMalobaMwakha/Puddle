2c679a6a878bbd04277fa8616b408f2f22588e56
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-07-26 01:47:05 +0300

    new file:   puddle/conversation/migrations/0003_alter_conversationmessage_content.py

--- puddle/core/models.py
+++ puddle/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, related_name='items', on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='profile_avatar', blank=True, null=True)
     location = models.CharField(max_length=255, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
