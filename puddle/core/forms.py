9741d44ea76f472084226f40c66702fdb691d24b
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-07-29 03:00:59 +0300

    modified:   puddle/core/__pycache__/forms.cpython-310.pyc modified:   puddle/core/forms.py

--- puddle/core/forms.py
+++ puddle/core/forms.py
@@ -37,8 +37,3 @@ class SignupForm(UserCreationForm):
     }))
 
 
-
-class ProfileEditForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['avatar', 'location', 'bio']
